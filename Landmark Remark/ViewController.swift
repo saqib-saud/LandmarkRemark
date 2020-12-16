04ad32c1f51ee6f0696df9da47c6cc8ede7816c4
Saqib Saud <saqibsaud@gmail.com>
2020-12-16 16:02:52 +1100

    Initial Commit

---
+++ Landmark Remark/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Landmark Remark
+//
+//  Created by Saqib Saud on 16/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
